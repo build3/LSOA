97adff479d37c4a6b420fe7d6f74b92129a41ff2
Andrew Webb <androbwebb@gmail.com>
2018-03-21 10:43:34 -0700

    SyncDB script

---
+++ sync_db.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export PGPASSWORD="a9c40c18fb6acb178ad14770cd995fc7df6cd277494c681b69f8221b03d7e204"
+pg_dump -h ec2-54-235-86-244.compute-1.amazonaws.com -p 5432 -Fc --no-acl --no-owner -o -U cjkjixfzqxvufd d3q8rv0h6cuhp4  | gzip > dumpfile.gz
+
+unset PGPASSWORD
+gunzip -c dumpfile.gz | pg_restore --verbose --clean --no-acl --no-owner -d lsoa db.dump -U lsoa
