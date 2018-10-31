7523c20ce748d15ce88db76cc3309bacc8c96e05
Rafał Kowalski <rafal.kowalski@makimo.pl>
2018-10-31 13:57:59 +0100

    Fit report v3 to optimized observation format

--- kidviz/templatetags/observations.py
+++ kidviz/templatetags/observations.py
@@ -5,24 +5,8 @@ register = template.Library()
 
 
 @register.filter
-def keyvalue(dict, key):
-    return dict.get(key)
-
-
-@register.filter
-def selected_constructs(tables, filtered):
-    if not filtered:
-        return []
-    filtered_ids = set(filtered.values_list('id', flat=True))
-    return [table for table in tables if any(id in table['construct_map'] for id in filtered_ids)]
-
-
-@register.filter
-def selected_subconstructs(constructs, filtered):
-    if not filtered:
-        return []
-    filtered_ids = set(filtered.values_list('id', flat=True))
-    return [(id, construct) for id, construct in constructs if id in filtered_ids]
+def contains(observations, construct):
+    return any(o.level.construct == construct for o in observations)
 
 
 @register.filter
