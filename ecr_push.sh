9efa2c36782809ef76ad87b5e8aad1b871409199
Chris Thompson <chris@233analytics.com>
2019-09-19 16:53:03 -0500

    added a jenkinsfile and updated the production deployment scripts for the AWS environment

---
+++ ecr_push.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+$(aws ecr get-login --no-include-email --region us-east-2)
+docker build -t tots . -f ./docker/production/Dockerfile
+docker tag tots:latest 617608773253.dkr.ecr.us-east-2.amazonaws.com/tots:latest
+docker push 617608773253.dkr.ecr.us-east-2.amazonaws.com/tots:latest
--- tools/run-production.sh
+++ tools/run-production.sh
@@ -5,7 +5,7 @@ set -e
 NAME="kidviz"
 NUM_WORKERS=5
 
-/tools/wait-for db:5432 -- echo Database ready
+#/tools/wait-for db:5432 -- echo Database ready
 
 DJANGO_SETTINGS_MODULE=settings
 DJANGO_WSGI_MODULE=wsgi
